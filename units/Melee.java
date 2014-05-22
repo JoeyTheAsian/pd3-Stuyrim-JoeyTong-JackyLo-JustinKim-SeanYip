96f9bad5d5dfbe4942aee978af50d1860a02672f
Jacky Lo <jljcky@aim.com>
2014-05-22 15:20:30 -0400

    Update Melee.java

--- units/Melee.java
+++ units/Melee.java
@@ -3,6 +3,20 @@ import java.io.*;
 
 public class Melee extends Player{
 
-
+  public Melee(){
+    //constructer
+  }
+  
+  public void sAttack(){
+    
+  }
+  
+  public void sAttack2(){
+    
+  }
+  
+  public void sAttack3(){
+    
+  }
 
 }
