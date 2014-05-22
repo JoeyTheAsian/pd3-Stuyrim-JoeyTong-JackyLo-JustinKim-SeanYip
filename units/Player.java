7e71c32747818276ff5abfa9d1770310c3ec134b
Jacky Lo <jljcky@aim.com>
2014-05-22 15:17:18 -0400

    Update Player.java

--- units/Player.java
+++ units/Player.java
@@ -1,8 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-public abstract class Player{
+public abstract class Player extends Unit{
  
-    
-   
+  //player should have three unique special attacks 
+  //NOTE: sAttack1() = sAttack() from Unit class
+  
+  public abstract void sAttack2();
+  
+  public abstract void sAttack3();
+  
+  //more methods to come
+  
 }
