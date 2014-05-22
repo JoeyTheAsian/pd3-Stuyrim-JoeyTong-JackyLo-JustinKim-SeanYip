cf80b04787894e5fa664f547a0400d3fa292f685
Sean Yip <bouowmx@gmail.com>
2014-05-21 21:01:14 -0400

    Added cryptographic hashing

--- sample/Utilities.java
+++ sample/Utilities.java
@@ -1,7 +1,17 @@
 import java.awt.Component;
+import java.io.File;
+import java.nio.file.Files;
+import java.security.MessageDigest;
 import javax.swing.JOptionPane;
+import javax.xml.bind.DatatypeConverter;
 
 public class Utilities {
+	public static String hash(String file, String algorithm) {
+		try {return DatatypeConverter.printHexBinary(MessageDigest.getInstance(algorithm).digest(Files.readAllBytes((new File(file)).toPath())));}
+		catch (Exception e) {e.printStackTrace();}
+		return "";
+	}
+	
 	public static void showErrorMessage(Component parent, Exception e) {JOptionPane.showMessageDialog(parent, "An error has occurred. Stack trace:\n\n" + stackTraceToString(e), "Error", JOptionPane.ERROR_MESSAGE);}
 	
 	public static String stackTraceToString(Throwable e) {
