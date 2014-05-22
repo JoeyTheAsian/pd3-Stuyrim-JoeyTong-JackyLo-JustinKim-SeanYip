72ac9b7c6ee8895773d0834a4b4efe8bd308206f
Jacky Lo <jljcky@aim.com>
2014-05-22 10:14:53 -0400

    added units

--- units/Monster.java
+++ units/Monster.java
@@ -1,52 +1,8 @@
 import java.util.*;
 import java.io.*;
 
-public abstract class Monster{
-
-    protected String name;
-    protected String desc;
-    protected int lvl;
-    protected int HP;
-    protected int ATK;
-    protected int DEF;
-    protected int luk;
-    protected int speed;
-    protected ArrayList<Item>() list = new ArrayList<Item>();
-    protected Image sprite, image;
-    protected int mana;
+public abstract class Monster extends Unit{
 
     public abstract void setList();
-
-    public String getName(){ return name; }
-
-    public String getDesc(){ return desc; }
-
-    public int getLVL(){ return lvl; }
-
-    public void setHP(int result){ hp = result; }
-
-    public int getHP(){ return HP; }
-
-    public int getATK(){ return ATK; }
-    
-    public int getDEF(){ return DEF; }
-
-    public int getLuk(){ return luk; }
-
-    public int getSpeed(){ return speed; }
-
-    public Image getSprite(){ return sprite; };
-
-    public Image getImage(){ return image; };
-
-    public int getMana(){ return mana; }
-
-    public void charge(){ mana+=10; }
-
-    public void attack(){
-
-    }
-
-    public abstract void sAttack(); //stands for special attack
 
 }
