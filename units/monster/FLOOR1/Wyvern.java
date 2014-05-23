deed0fd0b1a4b6550b78f853d6c964f3280afded
Jacky Lo <jljcky@aim.com>
2014-05-23 15:28:31 -0400

    Create Wyvern.java

---
+++ units/monster/FLOOR1/Wyvern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Wyvern extends Monster{
+
+  public Wyvern(){
+    name = "Wyvern";
+    desc = "Two-Legged Small Dragon";
+    sprite = new ImageIcon("WyvernSpr.png").getImage();
+    image = new ImageIcon("Wyern.png").getImage();
+  }
+
+  public void sAttack(){
+  
+  }
+
+}
