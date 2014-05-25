0701691d77e0b14b8a7559f5efcc7a6d527ef6ca
Joey Tong <joeyptong@yahoo.com>
2014-05-24 22:01:55 -0400

    modified:   sample/Menu.java 	new file:   sample/optionsButton.png 	new file:   sample/startButton.png

--- sample/Menu.java
+++ sample/Menu.java
@@ -1,10 +1,12 @@
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Color;
 import java.awt.Toolkit;
 import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.awt.Image;
 import java.io.File;
 import static java.lang.System.out;
 import java.util.HashMap;
@@ -14,11 +16,11 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
+import javax.swing.ImageIcon;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
-
+import javax.swing.SwingConstants;
 
 public class Menu extends JFrame {
     BufferedImage image;
@@ -60,11 +62,38 @@ public class Menu extends JFrame {
 	    
 	    //themeComboBox.setSize(100, 25);
 	    optionsButton.addActionListener(e -> {
+
 	      });
+	    optionsButton.setOpaque(false);
+	    optionsButton.setBorderPainted(false);
+	    optionsButton.setContentAreaFilled(false);
 	    optionsButton.setSize(width/10,height/20);
+	    optionsButton.setVerticalTextPosition(SwingConstants.CENTER);
+	    optionsButton.setHorizontalTextPosition(SwingConstants.CENTER);
+
 	    optionsButton.setLocation((width/3)-(width/20), (height/7*6)-(height/40));
+	    Image img = new ImageIcon("optionsButton.png").getImage().getScaledInstance
+		(optionsButton.getWidth(),optionsButton.getHeight(),java.awt.Image.SCALE_SMOOTH);
+	    optionsButton.setIcon(new ImageIcon(img));
+	    optionsButton.setName("Options");
+	    optionsButton.setForeground(Color.white);
+
+	    startButton.addActionListener(e -> {
+
+	      });
+	    startButton.setOpaque(false);
+	    startButton.setBorderPainted(false);
+	    startButton.setContentAreaFilled(false);
 	    startButton.setSize(width/10,height/20);
+	    startButton.setVerticalTextPosition(SwingConstants.CENTER);
+	    startButton.setHorizontalTextPosition(SwingConstants.CENTER);
+
 	    startButton.setLocation((width/3*2)-(width/20), (height/7*6)-(height/40));
+	    Image img1 = new ImageIcon("startButton.png").getImage().getScaledInstance
+		(optionsButton.getWidth(),optionsButton.getHeight(),java.awt.Image.SCALE_SMOOTH);
+	    startButton.setIcon(new ImageIcon(img1));
+	    startButton.setName("Start Game!");
+	    startButton.setForeground(Color.white);
 
 	    add(optionsButton);
 	    add(startButton);
