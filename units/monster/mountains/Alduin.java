d3247c488bce28af344db1a938f2625f35f5a43f
Jacky Lo <jljcky@aim.com>
2014-05-26 22:46:33 -0400

    Added more mountains monsters

---
+++ units/monster/mountains/Alduin.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Alduin extends Monster{
+
+    public Alduin(){
+	name = "Alduin";
+	HP = 10000;
+	mana = 500;
+	ATK = 500;
+	DEF = 250;
+	EXP = 10000;
+	speed = 50;
+	ATKspeed = 50;
+	image = new ImageIcon("Alduin.png").getImage();
+    }
+
+    public void setList(){
+	
+    }
+
+    public void sAttack(){
+
+    }
+
+    public void sAttack2(){
+	
+    }
+
+    public void sAttack3(){
+
+    }
+
+}
---
+++ units/monster/mountains/Dwarf.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Dwarf extends Monster{
+
+    public Dwarf(){
+	name = "Dwarf";
+	HP = 1500;
+	mana = 500;
+	ATK = 200;
+	DEF = 100;
+	EXP = 150;
+	speed = 5;
+	ATKspeed = 50;
+	image = new ImageIcon("Dwarf.png").getImage();
+    }
+
+    public void setList(){
+
+    }
+
+    public void sAttack(){
+
+    }
+
+}
