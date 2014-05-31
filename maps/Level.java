604c90ff8110d3303879a73c05fc820272cf5942
justinkim <yulli@yulli.org>
2014-05-31 16:50:42 -0400

    Add Tile enum and Level class

---
+++ maps/Level.java
@@ -0,0 +1,65 @@
+public class Level {
+
+    public static final int NUM_ROWS = 20;
+    public static final int NUM_COLS = 25;
+
+    private Tile[][] tilemap;
+
+    public Level() {
+        tilemap = new Tile[NUM_COLS][NUM_ROWS];
+        setAll(Tile.FLOOR);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < NUM_COLS; i++) {
+            for (int j = 0; j < NUM_ROWS; j++) {
+                sb.append(tilemap[i][j].id + " ";
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    public int getWidth() {
+        return tilemap[0].length;
+    }
+
+    public int getHeight() {
+        return tilemap.length;
+    }
+
+    public Tile[][] getTileMap() {
+        return tilemap;
+    }
+
+    public TilegetTile(int x, int y) {
+        return tilemap[x][y];
+    }
+
+    public void setTile(int x, int y, Tile tile) {
+        tilemap[x][y] = tile;
+    }
+
+    public void setAll(Tile tile) {
+        for (int i = 0; i < NUM_COLS; i++) {
+            for (int j = 0; j < NUM_ROWS; j++) {
+                setTile(i, j, tile);
+            }
+        }
+    }
+
+    public void setColumn(int x, Tile tile) {
+        for (int i = 0; i < tilemap[0].length; i++) {
+            setTile(x, i, tile);
+        }
+    }
+
+    public void setRow(int y, Tile tile) {
+        for (int i = 0; i < tilemap.length; i++) {
+            setTile(i, y, tile);
+        }
+    }
+}
---
+++ maps/Tile.java
@@ -0,0 +1,9 @@
+public enum Tile {
+    public int id;
+    public String texture;
+
+    Tile(int id, String texture) {
+        this.id = id;
+        this.texture = texture;
+    }
+}
