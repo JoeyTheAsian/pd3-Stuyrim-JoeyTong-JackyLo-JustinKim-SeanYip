75aac5c802c52c0f64fdafd65edd3a8ca62ecbc8
Jacky Lo <jljcky@aim.com>
2014-05-31 22:34:34 -0400

    Create Magma.java

---
+++ units/monster/volcano-3/Magma.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Magma extends Monster{
+
+  public Magma(){
+  
+  }
+  
+  public void setList(){
+  
+  }
+  
+  public void sAttack(){
+  
+  }
+  
+}
