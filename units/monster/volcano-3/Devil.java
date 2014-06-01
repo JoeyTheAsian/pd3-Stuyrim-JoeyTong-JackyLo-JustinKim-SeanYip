c677ccd08b9a081ae4b441461f86ed6302e64c91
Jacky Lo <jljcky@aim.com>
2014-05-31 22:25:03 -0400

    Create Devil.java

---
+++ units/monster/volcano-3/Devil.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Devil extends Monster{
+
+  public Devil(){
+  
+  }
+  
+  public void setList(){
+  
+  }
+  
+  public void sAttack(){
+  
+  }
+
+}
