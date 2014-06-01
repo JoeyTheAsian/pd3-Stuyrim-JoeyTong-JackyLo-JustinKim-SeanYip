1b1258760721f26e4f7a24e471e4d734b9c486ac
Jacky Lo <jljcky@aim.com>
2014-05-31 22:23:43 -0400

    Create Imp.java

---
+++ units/monster/volcano-3/Imp.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Imp extends Monster{
+
+  public Imp(){
+  
+  }
+  
+  public void setList(){
+  
+  }
+  
+  public void sAttack(){
+  
+  }
+}
