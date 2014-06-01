300ac5bad051e61421da47a949625ce51646da7d
Jacky Lo <jljcky@aim.com>
2014-05-31 22:26:30 -0400

    Create Skeleton.java

---
+++ units/monster/volcano-3/Skeleton.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Skeleton extends Monster{
+
+  public Skeleton(){
+  
+  }
+  
+  public void setList(){
+  
+  }
+  
+  public void sAttack(){
+  
+  }
+  
+}
