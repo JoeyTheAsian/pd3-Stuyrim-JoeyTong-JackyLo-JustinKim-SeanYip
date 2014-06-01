49bfebc985ec679e07c4b5b4025ac565d1c44f3c
Jacky Lo <jljcky@aim.com>
2014-05-31 22:32:35 -0400

    Create Ghost.java

---
+++ units/monster/volcano-3/Ghost.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Ghost extends Monster{
+
+  public Ghost(){
+  
+  }
+  
+  public void setList(){
+  
+  }
+  
+  public void sAttack(){
+  
+  }
+  
+}
