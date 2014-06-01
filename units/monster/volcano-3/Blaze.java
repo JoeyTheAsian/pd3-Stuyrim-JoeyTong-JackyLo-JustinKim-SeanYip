c4f90247945c8e78fc8c3ade0a9bf9f0fd681e2d
Jacky Lo <jljcky@aim.com>
2014-05-31 22:29:35 -0400

    Create Blaze.java

---
+++ units/monster/volcano-3/Blaze.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Blaze extends Monster{  //Minecraft Reference
+
+  public Blaze()[
+  
+  }
+  
+  public void setList(){
+  
+  }
+  
+  public void sAttack(){
+  
+  }
+  
+  
+}
