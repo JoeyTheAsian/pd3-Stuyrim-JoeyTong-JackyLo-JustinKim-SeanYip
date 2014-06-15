7f857bd5238cd6de73e883d976c590674ba12809
Jacky Lo <jljcky@aim.com>
2014-06-14 22:45:17 -0400

    Create Wyvern.java

---
+++ sample/Wyvern.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+public class Wyvern extends Player{
+  public Wyvern(){super();}
+  public Wyvern(String imageLocation, int x, int y){
+    super(imageLocation,x,y);
+  }
+}
